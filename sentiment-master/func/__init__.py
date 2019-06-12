eea1d5b250c5921bf811b1566e72318ce141d59c
RogerSinghChugh <35698080+RogerSinghChugh@users.noreply.github.com>
2019-06-12 23:09:59 +0530

    Delete __init__.py

--- sentiment-master/func/__init__.py
+++
@@ -1,6 +0,0 @@
-from func.datavil import datavil
-from func.sentimfunc import sentimfunc
-from func.blankcheck import blankck
-from func.classck import classck
-from func.likecount import likecount
-from func.heatMap import heatmap
